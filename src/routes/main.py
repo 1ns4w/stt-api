6c80fe74f8175ad2b354eea12cb2fe9f2ac72c3a
luis-designs <luislozano120xd@gmail.com>
2022-08-10 23:12:10 -0500

    Up refactor

--- src/routes/main.py
+++ src/routes/main.py
@@ -1,15 +1,17 @@
-import asyncio
+from fastapi import FastAPI, UploadFile, File, BackgroundTasks
 import speech_recognition as sr
 
 from database.database import Audio
 from utils.convert_to_wav import convert_to_wav
-from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
 @app.post("/upload")
-async def upload(file: UploadFile = File(...)):
-
+async def upload(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
+    async def insertData(audio_text2):
+        audio = Audio(audio_file_name="Pollito con papas", audio_text=audio_text2)
+        await audio.insert()
+        print("Termino de insertar")
     try:
         sound = convert_to_wav(file)
         sound.export('audio.wav', format="wav")
@@ -19,9 +21,10 @@ async def upload(file: UploadFile = File(...)):
 
         with file_audio as source:
             audio_text = r.record(source)
-            
-        audio = Audio(audio_file_name=file.filename, audio_text=r.recognize_google(audio_text))
-        asyncio.ensure_future(audio.insert())
+
+        audio_text2 = r.recognize_google(audio_text) 
+        
+        background_tasks.add_task(insertData, audio_text2)
 
         return {"success": "upload in progress", "file_name": file.filename}
 
