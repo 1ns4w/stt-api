4e293245723a147c3df64ad5dfc76dbeb1414b07
Angel Mejia <amejiadev@gmail.com>
2022-08-14 17:38:48 -0500

    xd

--- src/utils/convert_to_wav.py
+++ src/utils/convert_to_wav.py
@@ -1,7 +1,7 @@
 from pydub import AudioSegment
 from fastapi import UploadFile, File
 
-def convert_to_wav(file: UploadFile = File(...)):
+def convert_to_wav(file):
     contents = file.file.read()
 
     with open(file.filename, 'wb') as f:
@@ -9,7 +9,9 @@ def convert_to_wav(file: UploadFile = File(...)):
 
     try:
         sound = AudioSegment.from_mp3(file.filename)
+        print("Convirtiendo de mp3 a wav")
     except:
         sound = AudioSegment.from_file(file.filename, format="mp4")
+        print("Convirtiendo de mp4 a wav")
 
     return sound
