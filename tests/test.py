1fd86204b66c95cdebf163d61180a72eb2167d21
Angel Mejia <amejiadev@gmail.com>
2022-08-14 15:08:01 -0500

    xd

--- tests/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1:5050/api/upload'
-file = {'file': open('tests/audio.mp3', 'rb')}
-print('sending')
-resp = requests.post(url=url, files=file) 
-print(resp.json())
