6e28ac4e32eb0d221099a1a264b9eaefe4ff8f63
Angel Mejia <amejiadev@gmail.com>
2022-08-14 15:09:39 -0500

    Chore: Format comments

--- src/routes/main.py
+++ src/routes/main.py
@@ -6,19 +6,20 @@ router = APIRouter()
 
 @router.post("/upload")
 def upload(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
+    background_tasks.add_task(Conversor.insert_audio, file)
+    return {"success": "upload in progress", "file_name": file.filename}
 
-        background_tasks.add_task(Conversor.insert_audio, file)
-        return {"success": "upload in progress", "file_name": file.filename}
 
+"""
+def start() :
+    server = FastAPI()
 
-""" def start() :
-    sever = FastAPI()
-
-    @sever.post("/upload")
+    @server.post("/upload")
     async def upload(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
         try:
             background_tasks.add_task(Conversor.insert_audio, file)
             return {"success": "upload in progress", "file_name": file.filename}
 
         except Exception as e:
-            return {"error": e.message, "file_name": file.filename} """
+            return {"error": e.message, "file_name": file.filename}
+"""
