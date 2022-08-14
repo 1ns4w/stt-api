3cab01807d52e450bcf1ef8e724f5f8bbd27c5e5
Angel Mejia <amejiadev@gmail.com>
2022-08-14 15:50:30 -0500

    CHore

--- src/app.py
+++ src/app.py
@@ -7,6 +7,6 @@ PORT = environ.get("PORT")
 print(PORT)
 
 if __name__ == "__main__":
-    config = uvicorn.Config("services.server:app", port=PORT, HOST="0.0.0.0", log_level="info")
+    config = uvicorn.Config("services.server:app", port=PORT, host="0.0.0.0", log_level="info")
     server = uvicorn.Server(config)
     server.run()
